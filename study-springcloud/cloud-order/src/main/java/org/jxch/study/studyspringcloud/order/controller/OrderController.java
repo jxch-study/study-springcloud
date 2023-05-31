8ed6b2f17b8340892170e2e0cb3f16ef2e30c873
jxch <qianbuhan@proton.me>
2023-06-01 03:35:17 +0800

    gateway

---
+++ study-springcloud/cloud-order/src/main/java/org/jxch/study/studyspringcloud/order/controller/OrderController.java
@@ -0,0 +1,16 @@
+package org.jxch.study.studyspringcloud.order.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/order")
+public class OrderController {
+
+    @GetMapping("/get")
+    public String get() {
+        return "order";
+    }
+
+}
