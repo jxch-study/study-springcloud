d0489e9f4ef1a66564e853a58895b9162d227052
jxch <qianbuhan@proton.me>
2023-05-31 15:18:59 +0800

    App

--- study-springcloud/src/main/java/org/jxch/study/studyspringcloud/StudySpringCloudApplication.java
+++ study-springcloud/src/main/java/org/jxch/study/studyspringcloud/StudySpringCloudApplication.java
@@ -1,5 +1,11 @@
 package org.jxch.study.studyspringcloud;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class StudySpringCloudApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(StudySpringCloudApplication.class, args);
+    }
 }
