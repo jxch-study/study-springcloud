c9aceafc37575d716e856757436130267edb3ab5
jxch <qianbuhan@proton.me>
2023-06-02 21:50:03 +0800

    授权中心

---
+++ study-springcloud/cloud-auth/src/main/java/org/jxch/study/studyspringcloud/auth/AuthCenterApp.java
@@ -0,0 +1,12 @@
+package org.jxch.study.studyspringcloud.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class AuthCenterApp {
+    public static void main(String[] args) {
+        SpringApplication.run(AuthCenterApp.class, args);
+    }
+}
